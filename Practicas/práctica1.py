63809087bff0691ef983c1f0bd07b60bd3320e0d
luismario07hr <20255813@esen.edu.sv>
2025-10-06 09:59:49 -0600

    Prácticas
    Las primeras dos prácticas que nos dejo Alvin

---
+++ Practicas/práctica1.py
@@ -0,0 +1,19 @@
+nombre = "Luis Mario Hernández Ruíz"
+edad = 17
+ciudad = "Santa Ana"
+profesion = "Estudiante"
+mensaje = "Bienvenido al curso de python"
+anio_actual = 2025
+anio_nacimiento = 2006
+pi = 3.14159265359
+
+#Una variable almacena datos
+
+print (mensaje)
+print (f"Mi nombre es {nombre}, tengo {edad}. Soy de {ciudad} y me dedico a ser {profesion}")
+print (anio_actual - anio_nacimiento)
+print (f"El valor aproximado de pi es {pi:.4f}")
+
+"""La variable de tipo entero almacena únicamente datos númericos
+entero, no decimales. En cambio, la de tipo cadena logra almacenar
+textos como palabras, frase, etc."""
