39ca2b02c079b144d651428711c3180abdd97760
luismario07hr <20255813@esen.edu.sv>
2025-10-07 17:04:44 -0600

    clase

---
+++ Clase4/clase7-10-2025.py
@@ -0,0 +1,8 @@
+nombre = "Alvin"
+palabra = "RECONOCER"
+
+"""print (len(nombre))
+print (nombre[1:])"""
+
+print (len(palabra))
+print (palabra [0::2])
---
+++ Practicas/práctica3.py
@@ -0,0 +1,33 @@
+#Crea tres variables: a = 10, b = 3, c = 5. Calcula y muestra: la suma de a y b, el resultado de a dividido entre b, el residuo de dividir a entre c.
+a = 10
+b = 3
+c = 5
+d = a + b
+
+print (d)
+print (d/3)
+print (a/c)
+
+#Crea una variable numero con valor 7 y muestra True si es mayor que 5, False en caso contrario.
+
+num = 4
+condicional = num > 5
+
+print (condicional)
+
+#Crea dos variables x = 10, y = 10. Usa los operadores is e is not para verificar si apuntan al mismo objeto en memoria.
+x = 10
+y = 10
+veri = x is y
+veri2 = x is not y
+
+print (veri)
+print (veri2)
+
+#Pide un número al usuario y muestra True si es par, False si es impar (usa el operador %).
+
+
+#Crea una variable booleana registrado = True. Usa operadores lógicos (and, or, not) para mostrar combinaciones posibles.
+#Pide al usuario dos números y muestra si el primero es mayor o igual que el segundo.
+#Pide al usuario su edad y muestra si tiene edad suficiente para votar (mayor o igual que 18).
+#Crea un ejemplo que combine operadores relacionales y lógicos, como: edad = 22; estudiante = True; print(edad >= 18 and estudiante).
