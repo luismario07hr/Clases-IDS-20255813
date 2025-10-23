cea796a3c18780348a5e1c09e3ed1face7c053f9
luismario07hr <20255813@esen.edu.sv>
2025-10-23 17:16:56 -0600

    clase

---
+++ Clase.py
@@ -0,0 +1,21 @@
+monto = float(input("Digite el mondo: "))
+tipo = input("Tipo (local/internacional): ") 
+
+if tipo.lower() == "local": 
+    if monto > 100: 
+        print ("7%")
+    elif monto > 75: 
+        print ("5%")
+    else: 
+        print ("0%")
+
+elif tipo.lower() == "internacional": 
+    if monto > 100: 
+        print ("12%")
+    elif monto > 75: 
+        print ("9%")
+    else: 
+        print ("0%")
+
+else: 
+    print ("estupido elegí las opciones que te damos no te inventes lo que querras")
